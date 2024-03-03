bb24a3a9d29eda14a05bd4e621cad6089c3cd2a6
emo-mruczek <110098604+emo-mruczek@users.noreply.github.com>
2024-03-03 22:00:39 +0100

    add: speakers

--- src/assets/speakers/speakersData.ts
+++ src/assets/speakers/speakersData.ts
@@ -72,4 +72,82 @@ export const SPEAKERS: SpeakerProps[] = [
       "The importance of thermal control has been growing since the outset of computing and it becomes critical as the sizes of systems get ever smaller and they are more and more constrained by their form factors. In contemporary systems, it is often impossible to run all of the components at full capacity at the same time and thermal control scenarios involve constraining performance and distributing power, so as to prevent the system from getting too hot while still allowing the users' expectations to be met. Many of these systems run the Linux kernel and it has to adequately respond to the thermal control challenges, which will be the focus of my talk. I will describe the Linux kernel's thermal control subsystem and explain how it works. I will also talk about the possible directions of its development in the future related to some interesting use cases that need to be addressed.",
     ],
   },
+  {
+    image: blankAvatar,
+    urls: {
+      website: '',
+      twitter: '',
+      linkedin: '',
+      github: '',
+    },
+    name: 'Maciej Lasyk',
+    description: [
+      '',
+    ],
+  },
+  {
+    image: blankAvatar,
+    urls: {
+      website: '',
+      twitter: '',
+      linkedin: '',
+      github: '',
+    },
+    name: 'Filip Dorosz',
+    description: [
+      'DevOps Engineer with passion. My main interest lies in SRE and observability. I really do believe in Open Source software. While debugging performance issues I mainly listen to Synthwave music and Neue Deutsche Härte',
+    ],
+  },
+  {
+    image: blankAvatar,
+    urls: {
+      website: '',
+      twitter: '',
+      linkedin: '',
+      github: '',
+    },
+    name: 'Patryk Wlazłyń',
+    description: [
+      'Software engineer, firmware, drivers and everything that has to be small or run fast.',
+    ],
+  },
+  {
+    image: blankAvatar,
+    urls: {
+      website: '',
+      twitter: '',
+      linkedin: '',
+      github: '',
+    },
+    name: 'Dominik Mierzejewski',
+    description: [
+      'Long time open source contributor. Fedora user and developer. Maintains over 100 software packages and holds various community roles, including being a Fedora ambassador, packager sponsor and proven packager.',
+    ],
+  },
+  {
+    image: blankAvatar,
+    urls: {
+      website: '',
+      twitter: '',
+      linkedin: '',
+      github: '',
+    },
+    name: 'Piotr Pyciński',
+    description: [
+      '15-letnie doświadczenie jako administrator systemów. Moje początki miały miejsce w czasach, gdy praca przy konsoli i fizycznych serwerach było standardem, a szum wentylatorów towarzyszył codziennym czynnościom. Obecnie staram się odnaleźć w świecie bujającym w chmurach i często podkreślam, że nie jest to uniwersalne rozwiązanie dla wszystkich problemów. Poza pracą lubię żeglować po morzach i oceanach oraz pracuję jako ratownik medyczny w Krakowskim Pogotowiu Ratunkowym.',
+    ],
+  },
+  {
+    image: blankAvatar,
+    urls: {
+      website: '',
+      twitter: '',
+      linkedin: '',
+      github: '',
+    },
+    name: 'Krzysztof Biegański',
+    description: [
+      'Krzysztof is a Senior Software Engineer at Antmicro. In his everyday work, he mostly focuses on open source SystemVerilog tooling. He is one of the main contributors to Verilator, responsible for, among other things, extending the scheduler in Verilator to support timing constructs.',
+    ],
+  },
 ];
