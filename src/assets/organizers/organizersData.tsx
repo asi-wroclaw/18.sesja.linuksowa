36e8adbd01ded75b5c5a168524881bf239e4ab46
emo.mruczek <krokcia1@gmail.com>
2024-04-05 22:37:15 +0200

    add: photos

--- src/assets/organizers/organizersData.tsx
+++ src/assets/organizers/organizersData.tsx
@@ -1,47 +1,48 @@
 import blankAvatar from './blank-avatar.jpg';
 import { OrganizerProps } from '@/slices/Organizers';
 import { useTranslation } from 'next-export-i18n';
-
-function getTranslation() {
-  const { t } = useTranslation('common');
-  return t('team');
-}
+import Michal from "./michal.png";
+import Tomek from "./rikardo.png";
+import Jakub from "./sylwek.png";
+import Jacek from "./jackek.png";
+import Felix from "./felix.png";
+import Ola from "./ola.jpeg";
 
 export const ORGANIZERS: OrganizerProps[] = [
 
 
   {
-    image: blankAvatar,
+    image: Michal,
     name: 'Michał Korczak',
-    role: getTranslation(),
+    role: 'Organizator Główny, Mistrz Ceremonii',
   },
   {
-    image: blankAvatar,
+    image: Tomek,
     name: 'Tomasz Świątek',
     role: "Organizator ds. Sponsorów",
   },
   {
-    image: blankAvatar,
+    image: Jakub,
     name: 'Jakub Śliwka',
     role: "Organizator ds. Prelegentów",
   },
   {
-    image: blankAvatar,
+    image: Jacek,
     name: 'Jacek Poziemski',  
     role: "Organizator ds. Prelegentów",
   },
   {
-    image: blankAvatar,
+    image: Ola,
     name: 'Aleksandra Mokrzan',  
     role: "Organizatorka ds. Marketingu",
   },
+ // {
+ //   image: blankAvatar,
+ //   name: 'Wiktor Rojecki',
+ //   role: "Organizator ds. Marketingu",
+ // },
   {
-    image: blankAvatar,
-    name: 'Wiktor Rojecki',  
-    role: "Organizator ds. Marketingu",
-  },
-  {
-    image: blankAvatar,
+    image: Felix,
     name: 'Felix Zieliński',  
     role: "Organizator ds. Strony",
   },
