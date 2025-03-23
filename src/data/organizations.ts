caab9adf11efc77a938e3f4015cd5e246a2f05e8
Michał Korczak <michal.k2001@gmail.com>
2025-03-23 21:06:32 +0100

    fix: asi url

--- src/data/organizations.ts
+++ src/data/organizations.ts
@@ -15,7 +15,7 @@ import type { SponsorshipEntity } from '@/components/sponsorship/types';
 export const ORGANIZATIONS = [
   {
     title: 'organizers',
-    logos: [{ image: asi, href: 'https://www.asi.wroclaw.pl/' }],
+    logos: [{ image: asi, href: 'https://asi.wroclaw.pl/' }],
   },
   {
     title: 'sponsors',
